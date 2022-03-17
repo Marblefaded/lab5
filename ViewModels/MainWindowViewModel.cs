aa3d922ee583f7bcdb4cab04e70a07812466700b
Marblefade <sitnikov.vladimiir@mail.ru>
2022-03-17 14:38:08 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reactive;
+using ReactiveUI;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace lab5.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        string text = "";
+        string text2 = "";
+        string patternold = "";
+        string patternnew = "";
+
+        public string TextIn
+        {
+            get { return text; }
+
+            set
+            {
+                this.RaiseAndSetIfChanged(ref text, value);
+                TextOut = "";
+                foreach (Match match in Regex.Matches(text, patternnew))
+                {
+                    TextOut += $"{match.Value}\n";
+                }
+
+            }
+        }
+        public string TextOut
+        {
+            get { return text2; }
+
+            set
+            {
+                this.RaiseAndSetIfChanged(ref text2, value);
+            }
+        }
+
+        string pathin = "";
+
+        public string SetPath
+        {
+            get => pathin;
+
+            set
+            {
+                pathin = value;
+                TextIn = File.ReadAllText(pathin);
+            }
+        }
+
+        string pathout = "";
+
+        public string GetPath
+        {
+            get => pathout;
+
+            set
+            {
+                pathout = value;
+                File.AppendAllText(pathout, text2);
+            }
+        }
+
+
+        public string PatternOld
+        {
+            get => patternold;
+
+            set
+            {
+                patternold = value;
+            }
+        }
+
+        public string PatternNew
+        {
+            get => patternnew;
+
+            set
+            {
+                patternnew = value;
+                TextOut = "";
+                foreach (Match match in Regex.Matches(text, patternnew))
+                {
+                    TextOut += $"{match.Value}\n";
+                }
+            }
+        }
+
+    }
+
+
+
+}
---
+++ Views/RegexWindow.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using lab5.ViewModels;
+using Avalonia.Interactivity;
+
+namespace lab5.Views
+{
+    public partial class RegexWindow : Window
+    {
+        public RegexWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.FindControl<Button>("OKButton").Click += async delegate
+            {
+                var contex = this.DataContext as MainWindowViewModel;
+                contex.PatternNew = contex.PatternOld;
+            };
+
+            this.FindControl<Button>("CancelButton").Click += async delegate
+            {
+                var contex = this.DataContext as MainWindowViewModel;
+                contex.PatternOld = contex.PatternNew;
+                Close();
+            };
+        }
+
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
